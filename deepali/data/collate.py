a3e467c9e9f729595d706941e6ca232bc7331d5b
Andreas Schuh <aschuh@heartflow.com>
2021-06-05 23:59:35 +0100

    enh: collate_samples() to support FlowField and FlowFields

--- deepali/data/collate.py
+++ deepali/data/collate.py
@@ -10,6 +10,7 @@ from torch.utils.data.dataloader import default_collate
 
 from ..core.types import Batch, Dataclass, Sample, is_namedtuple
 
+from .flow import FlowField, FlowFields
 from .image import Image, ImageBatch
 from .sample import sample_field_names, sample_field_value
 from .sample import replace_all_sample_field_values
@@ -57,11 +58,29 @@ def collate_samples(batch: Sequence[Sample]) -> Batch:
             samples.append(value)
         if is_none:
             values.append(None)
+        elif isinstance(elem0, FlowField):
+            flow_fields: List[FlowField] = samples
+            if any(flow_field.axes() != elem0.axes() for flow_field in flow_fields):
+                raise ValueError(
+                    f"collate_samples() 'batch' contains '{name}' flow fields with mixed axes"
+                )
+            data = default_collate([flow_field.tensor() for flow_field in flow_fields])
+            grid = tuple(flow_field.grid() for flow_field in flow_fields)
+            values.append(FlowFields(data, grid, elem0.axes()))
+        elif isinstance(elem0, FlowFields):
+            flow_fields: List[FlowFields] = samples
+            if any(flow_field.axes() != elem0.axes() for flow_field in flow_fields):
+                raise ValueError(
+                    f"collate_samples() 'batch' contains '{name}' flow fields with mixed axes"
+                )
+            data = torch.cat([flow_field.tensor() for flow_field in flow_fields], dim=0)
+            grid = tuple(grid for flow_field in flow_fields for grid in flow_field.grids())
+            values.append(FlowFields(data, grid, elem0.axes()))
         elif isinstance(elem0, Image):
             images: List[Image] = samples
             data = default_collate([image.tensor() for image in images])
-            grid = [image.grid() for image in images]
-            values.append(ImageBatch(data, grid=grid))
+            grid = tuple(image.grid() for image in images)
+            values.append(ImageBatch(data, grid))
         elif isinstance(elem0, ImageBatch):
             images: List[ImageBatch] = samples
             data = torch.cat([image.tensor() for image in images], dim=0)
