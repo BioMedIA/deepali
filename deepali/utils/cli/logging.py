dc95bb8a9682da36c38d8825e6ad05259a44bd28
Andreas Schuh <aschuh@heartflow.com>
2021-07-11 13:18:24 +0100

    enh: Add LogLevel enumeration for use with Typer

--- deepali/utils/cli/logging.py
+++ deepali/utils/cli/logging.py
@@ -1,6 +1,20 @@
 r"""Auxiliary functions to set up logging in main scripts."""
 
+from enum import Enum
 import logging
+
+
+class LogLevel(str, Enum):
+    r"""Enumeration of logging levels for use as type annotation when using Typer."""
+
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+
+    def __int__(self) -> int:
+        r"""Cast enumeration to int logging level."""
+        return int(getattr(logging, self.value))
 
 
 def configure_logging(log, args):
