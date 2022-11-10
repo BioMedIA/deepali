afb6f27799309080b811204999cb2724212f854b
Andreas Schuh <aschuh@heartflow.com>
2022-11-10 23:24:05 +0000

    enh: Add remove_layers_in_state_dict() and enable removing prefix when renaming layers

--- src/deepali/modules/utilities.py
+++ src/deepali/modules/utilities.py
@@ -1,7 +1,7 @@
 r"""Auxiliary functions for torch.nn.Module instances."""
 
-from collections import defaultdict
-from typing import Any, Dict, List, Optional
+from collections import OrderedDict, defaultdict
+from typing import Any, Dict, List, Mapping, Optional
 
 from torch.nn import Module
 
@@ -65,15 +65,44 @@ def module_counts_by_class_name(
     return {k: len(v) for k, v in ids.items()}
 
 
-def rename_layers_in_state_dict(state: Dict[str, Any], rename: Dict[str, str]) -> Dict[str, Any]:
+def remove_layers_in_state_dict(state: Mapping[str, Any], prefix: str) -> Dict[str, Any]:
+    r"""Remove layers in loaded state dict."""
+    metadata = getattr(state, "_metadata", None)
+    result = OrderedDict(state)  # does not copy _metadata attribute
+    if metadata is not None:
+        metadata = remove_layers_in_state_dict(metadata, prefix)
+        setattr(result, "_metadata", metadata)
+    for key in state.keys():
+        if key == prefix:
+            result.pop(key)
+        elif key.startswith(prefix + "."):
+            result.pop(key)
+    return result
+
+
+def rename_layers_in_state_dict(
+    state: Mapping[str, Any],
+    rename: Optional[Mapping[str, str]] = None,
+    parent: Optional[str] = None,
+) -> Dict[str, Any]:
     r"""Rename layers in loaded state dict."""
     metadata = getattr(state, "_metadata", None)
-    state = state.copy()  # does not copy _metadata attribute
+    state = OrderedDict(state)  # does not copy _metadata attribute
     if metadata is not None:
-        state._metadata = rename_layers_in_state_dict(metadata, rename)
-    for prefix, new_name in rename.items():
-        for key in list(state.keys()):
-            if key == prefix or key.startswith(prefix + "."):
-                new_key = new_name + key[len(prefix) :]
-                state[new_key] = state.pop(key)
+        metadata = rename_layers_in_state_dict(metadata, rename=rename, parent=parent)
+        setattr(state, "_metadata", metadata)
+    if rename:
+        for prefix, new_name in rename.items():
+            for key in list(state.keys()):
+                if key == prefix:
+                    value = state.pop(key)
+                    if new_name:
+                        state[new_name] = value
+                elif key.startswith(prefix + "."):
+                    value = state.pop(key)
+                    new_key = new_name + key[len(prefix) + (0 if new_name else 1) :]
+                    if new_key:
+                        state[new_key] = value
+    if parent:
+        state = {f"{parent}.{key}": value for key, value in state.items()}
     return state
